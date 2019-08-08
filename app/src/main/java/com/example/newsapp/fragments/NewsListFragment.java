6ed8e2db8a1e9436da4d72454f8642a45a606111
GentleSmile <478691929@qq.com>
2019-08-08 16:26:27 +0800

    20190808:16:26

--- app/src/main/java/com/example/newsapp/fragments/NewsListFragment.java
+++ app/src/main/java/com/example/newsapp/fragments/NewsListFragment.java
@@ -6,19 +6,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-//import androidx.fragment.app.Fragment
 
 import com.aspsine.irecyclerview.IRecyclerView;
 import com.aspsine.irecyclerview.OnLoadMoreListener;
 import com.aspsine.irecyclerview.OnRefreshListener;
+import com.aspsine.irecyclerview.animation.ScaleInAnimation;
 import com.aspsine.irecyclerview.widget.LoadMoreFooterView;
 
 import com.example.newsapp.R;
+import com.example.newsapp.adapters.NewsListAdapter;
+import com.example.newsapp.bean.AppConstant;
 import com.example.newsapp.bean.NewsSummary;
 import com.example.newsapp.adapters.NewListAdapter;
 
@@ -35,26 +38,27 @@ import butterknife.Bind;
  * Created by xsf
  * on 2016.09.17:30
  */
-public class ListFragment extends Fragment implements OnRefreshListener, OnLoadMoreListener {
-    @Bind(R.id.irc)
+public class NewsListFragment extends Fragment implements OnRefreshListener, OnLoadMoreListener {
+//    @Bind(R.id.irc)
     IRecyclerView irc;
     @Bind(R.id.loadedTip)
     LoadingTip loadedTip;
-    private NewListAdapter newListAdapter;
+    private NewsListAdapter newsListAdapter;
     private List<NewsSummary> datas = new ArrayList<>();
 
     private String mNewsId;
     private String mNewsType;
     private int mStartPage=0;
+//    Context a;
 
     // 标志位，标志已经初始化完成。
     private boolean isPrepared;
     private boolean isVisible;
 
-    public ListFragment() {}
+    public NewsListFragment() {}
 
-    public static ListFragment newInstance(String channel) {
-        ListFragment fragment = new ListFragment();
+    public static NewsListFragment newInstance(String channel) {
+        NewsListFragment fragment = new NewsListFragment();
         Bundle bundle = new Bundle();
         bundle.putString("channel", channel);
         fragment.setArguments(bundle);
@@ -70,7 +74,7 @@ public class ListFragment extends Fragment implements OnRefreshListener, OnLoadM
 
     @Override
     public void onRefresh() {
-        newListAdapter.getPageBean().setRefresh(true);
+        newsListAdapter.getPageBean().setRefresh(true);
         mStartPage = 0;
         //发起请求
         irc.setRefreshing(true);
@@ -79,7 +83,7 @@ public class ListFragment extends Fragment implements OnRefreshListener, OnLoadM
 
     @Override
     public void onLoadMore(View loadMoreView) {
-        newListAdapter.getPageBean().setRefresh(false);
+        newsListAdapter.getPageBean().setRefresh(false);
         //发起请求
         irc.setLoadMoreStatus(LoadMoreFooterView.Status.LOADING);
 //        mPresenter.getNewsListDataRequest(mNewsType, mNewsId, mStartPage);
@@ -88,16 +92,50 @@ public class ListFragment extends Fragment implements OnRefreshListener, OnLoadM
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+//            mNewsId = getArguments().getString(AppConstant.NEWS_ID);
+//            mNewsType = getArguments().getString(AppConstant.NEWS_TYPE);
+        }
+//        Context a = getContext();
+//        System.out.println(getContext());
+
         irc.setLayoutManager(new LinearLayoutManager(getContext()));
+        datas.clear();
+        newsListAdapter = new NewsListAdapter(getContext(), datas);
+        newsListAdapter.openLoadAnimation(new ScaleInAnimation());
+        irc.setAdapter(newsListAdapter);
+        irc.setOnRefreshListener(this);
+        irc.setOnLoadMoreListener(this);
+//        //数据为空才重新发起请求
+//        if(newListAdapter.getSize()<=0) {
+//            mStartPage = 0;
+//            mPresenter.getNewsListDataRequest(mNewsType, mNewsId, mStartPage);
+//        }
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_list, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_news_list, container, false);
+        irc = view.findViewById(R.id.news_irc);
         return view;
     }
 
+    public void returnNewsListData(List<NewsSummary> newsSummaries) {
+        if (newsSummaries != null) {
+            mStartPage += 20;
+            if (newsListAdapter.getPageBean().isRefresh()) {
+                irc.setRefreshing(false);
+                newsListAdapter.replaceAll(newsSummaries);
+            } else {
+                if (newsSummaries.size() > 0) {
+                    irc.setLoadMoreStatus(LoadMoreFooterView.Status.GONE);
+                    newsListAdapter.addAll(newsSummaries);
+                } else {
+                    irc.setLoadMoreStatus(LoadMoreFooterView.Status.THE_END);
+                }
+            }
+        }
+    }
 }
