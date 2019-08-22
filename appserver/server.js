e99c057169c781ab1599aa7473ad2ccdf4404b77
function2-llx <function2@qq.com>
2019-08-22 20:09:10 +0800

    server code

--- appserver/server.js
+++ appserver/server.js
@@ -4,8 +4,11 @@ const app = express()
 const url = require('url')
 const fs = require('fs')
 var bodyParser = require('body-parser')
+const superagent = require('superagent')
 
-app.use(express.static(path.join(__dirname, 'site')))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({//处理以form表单的提交
     extended: true
@@ -15,20 +18,22 @@ app.listen(8080, () => {
     console.log('App listening at port 8080')
 })
 
-app.post('/', (req, res) => {
-    data = req["body"]
-    console.log(data instanceof Object)
-    res.json({ lqb: "woyezheyangjuede" })
+app.get('/news', (req, appres) => {
+    console.log(req.url)
+    data = url.parse(req.url, true).query
+    console.log(data)
+    superagent.get('https://api2.newsminer.net/svc/news/queryNewsList')
+        .query({startDate: data.publishTime, endDate: data.publishTime})
+        .end((err, res) => {
+            console.log(res.body)
+            for (let news of res.body.data) {
+                if (news.newsID == data.newsID) {
+                    appres.render("index.ejs", news)
+                    break
+                }
+            }
+        })
 })
-
-// app.get('/get', (req, res) => {
-//     console.log(req.url)
-//     console.log(url.parse(req.url, true).query)
-//     res.send(fs.readFileSync('site/index.html').toString())
-//     // res.send('test')
-    
-//     // res.end()
-// })
 
 // app.get('/', (req, res, next) => {
 //     console.log('in get /', req.url)
